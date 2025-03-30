df873db542bcae71f08d6da837a43b4ca571c7d7
Ruslan Rodionov <ruslan.rodionow98@gmail.com>
2025-03-31 00:24:31 +0530

    fix: content type

--- app/routes/api.proxy.ts
+++ app/routes/api.proxy.ts
@@ -6,8 +6,8 @@ import { json } from "@remix-run/node";
 
 export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url);
-  const fileName = url.searchParams.get("file"); // Получаем имя файла
-  const allowedFiles = ["chat.js", "chat.css"]; // Ограничиваем доступ
+  const fileName = url.searchParams.get("file");
+  const allowedFiles = ["chat.js", "chat.css"];
 
   if (!fileName || !allowedFiles.includes(fileName)) {
     return json({ error: "Unauthorized" }, { status: 403 });
@@ -21,7 +21,7 @@ export const loader: LoaderFunction = async ({ request }) => {
 
     const contentType = fileName.endsWith(".css")
       ? "text/css"
-      : "application/javascript";
+      : "text/javascript";
 
     // @ts-ignore
     return new Response(createReadStream(filePath), {
